4c32fa8c453293745af33b643fc26fe08308ce5a
udaykca <140484793+udaykca@users.noreply.github.com>
2024-01-19 20:22:13 +0530

    Create alb_controller_role_policy.tf

---
+++ alb_controller_role_policy.tf
@@ -0,0 +1,14 @@
+variable "alb_role_polices" {
+   "team-policy-name" : [
+      {
+        sid    = "sid_name"
+        effect = "Allow"
+        actions = [
+              
+        ]
+        resources = [
+        ]
+        condition = []
+      }
+    ]
+}
