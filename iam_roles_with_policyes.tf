ae3663c6767ead92f3874e540944e96f70e84b93
udaykca <140484793+udaykca@users.noreply.github.com>
2024-01-19 20:23:38 +0530

    Create iam_roles_with_policyes.tf

---
+++ iam_roles_with_policyes.tf
@@ -0,0 +1,12 @@
+variable "iam_roles_with_policies" {
+"role_name" : [
+      {
+        sid       = "sid_name"
+        effect    = "Allow"
+        resources = [" "]
+        actions = [
+        ]
+        condition = []
+      }
+    ]
+}
